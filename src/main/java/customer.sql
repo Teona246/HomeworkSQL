8a43438a9e15af35254eef565a936d1255c941ef
Teona46 <teonika2406@gmail.com>
2025-01-19 17:57:40 +0300

    done

---
+++ src/main/java/customer.sql
@@ -0,0 +1,5 @@
+CREATE TABLE customer(
+"id" BIGINT,
+"name" VARCHAR(200),
+"type" VARCHAR(30)
+);
---
+++ src/main/java/orders.sql
@@ -0,0 +1,6 @@
+CREATE TABLE orders(
+"id" BIGINT,
+"customer_id" BIGINT,
+"product_id" BIGINT,
+"category" VARCHAR(200)
+);
